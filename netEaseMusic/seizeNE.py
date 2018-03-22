0abf3981808d270b8b1087fffaf05c950abef82c
Seta <sweater@azusasoft.com>
2018-03-22 20:14:38 +0800

    换用第三方接口

--- netEaseMusic/seizeNE.py
+++ netEaseMusic/seizeNE.py
@@ -8,11 +8,13 @@ import requests
 
 # http://music.163.com/api/v3/playlist/detail?id=512808550
 
-HOST_NET_EASE = "http://music.163.com/"
-API_PLAYLIST_DETAIL = "/api/v3/playlist/detail"
+HOST_NET_EASE = "https://api.imjad.cn/cloudmusic"
+API_PLAYLIST_DETAIL = ""
+# HOST_NET_EASE = "http://music.163.com/"
+# API_PLAYLIST_DETAIL = "/api/v3/playlist/detail"
 api_url = HOST_NET_EASE + API_PLAYLIST_DETAIL
 id = raw_input("请输入歌单 id: ")
-kwargs = {'id': id}
+kwargs = {'id': id, 'type': 'playlist'}
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate',
@@ -23,7 +25,8 @@ headers = {
     'Upgrade-Insecure-Requests': '1',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}
 print "Start request..."
-request = requests.get(api_url, kwargs, headers=headers)
+# request = requests.get(api_url, kwargs, headers=headers)
+request = requests.get(api_url, kwargs)
 print "Request : %s" % request.url
 print "Request complete, %s" % request
 jsonResult = request.json()
@@ -35,7 +38,8 @@ print "歌单名称:{}".format(list_name)
 
 
 def filter_img(item):
-    img_name = "{}_cover.{}".format(item['al']['name'].encode('gbk', 'ignore'), item['al']['picUrl'].split('.')[-1])
+    img_name = "{}_{}.{}".format(item['al']['name'].encode('gbk', 'ignore').replace('\\', '_').replace('/', '_'),
+                                 datetime.now().microsecond, item['al']['picUrl'].split('.')[-1])
     return {'name': img_name, 'url': item['al']['picUrl']}
 
 
@@ -58,8 +62,8 @@ for img_bean in img_beans:
     file_name = img_bean['name']
     file_url = img_bean['url']
     print "Downloading {}/{} : {}".format(downloaded + 1, total, file_name)
-    urllib.urlretrieve(file_url, dir_path + "\\" + file_name)
+    urllib.urlretrieve(file_url, "{}\{}".format(dir_path, file_name))
     downloaded += 1
-print "下载完成!\n已保存在\{}".format(dir_path)
+print "下载完成{}/{}!\n已保存在\{}".format(total, downloaded, dir_path)
 to_open_path = "{}\{}".format(os.getcwd(), dir_path)
 subprocess.Popen(r'explorer /select, {}'.format(to_open_path))
