66d7233ba16b4e666a87f0e0886222bfa91c265b
Seta <sweater@azusasoft.com>
2019-02-12 16:26:28 +0800

    無駄無駄無駄無駄

--- encodingTest/EncodingTest.py
+++ encodingTest/EncodingTest.py
@@ -1,9 +1,13 @@
 # -*- coding: UTF-8 -*-
 import sys
+import chardet
 
-reload(sys)
-sys.setdefaultencoding('ascii')
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
 
+s = "测试"
 print type("测试")
 print "测试"
+print chardet.detect(s)
+print unicode(s.decode(chardet.detect(s)["encoding"]))
 # print u"测试".decode().encode("utf-8")
